3d6fb593f3a1e5ec88977b6fd9dd5482a4452b3c
Md: Mohon <mdmohon6145@gmail.com>
2022-12-18 11:02:53 +0600

    react carosal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import image from "./images/download.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* npm i react-responsive-carousel */}
+      <Carousel
+        className="react-carosal-cus"
+        autoPlay={true}
+        stopOnHover={false}
+        infiniteLoop={true}
+        showThumbs={false}
+        transitionTime={1000}
+        // width="500px"
+      >
+        <div>
+          <img src={image} />
+          <h1 className="legend">Hello World</h1>
+        </div>
+        <div>
+          <img src={image} />
+          <h1 className="legend">Hello World</h1>
+        </div>
+        <div>
+          <img src={image} />
+          <h1 className="legend">Hello World</h1>
+        </div>
+      </Carousel>
     </div>
   );
 }
